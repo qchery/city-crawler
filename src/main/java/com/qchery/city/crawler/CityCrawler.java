04cde8cd590fcbe9dffd3e86e97b28fe44f92646
Chery <chinrui1016@163.com>
2018-07-30 19:24:12 +0800

    1. 修复省份提取失败的问题 2. 更新为最新年份的行政区划

--- src/main/java/com/qchery/city/crawler/CityCrawler.java
+++ src/main/java/com/qchery/city/crawler/CityCrawler.java
@@ -3,6 +3,7 @@ package com.qchery.city.crawler;
 import com.qchery.city.crawler.entity.Area;
 import com.qchery.city.crawler.enums.ExecutiveLevel;
 import com.qchery.city.crawler.pipeline.AreaMapperPipeline;
+import org.apache.http.HttpHeaders;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
@@ -20,7 +21,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class CityCrawler {
 
-    private static final String PREFIX_URL = "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2016/";
+    private static final String PREFIX_URL = "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2017/";
 
     private static final String INDEX_URL = PREFIX_URL + "index.html";
     private static final String CITY_REGEX_URL = PREFIX_URL + "\\d+.html";
@@ -30,7 +31,7 @@ public class CityCrawler {
         InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
         Spider.create(new StatsGovPageProcessor())
-                .addUrl(INDEX_URL)
+                .addRequest(createRequest(INDEX_URL))
                 .thread(8)
                 .addPipeline(new AreaMapperPipeline(sqlSessionFactory)).run();
     }
@@ -47,7 +48,7 @@ public class CityCrawler {
         public void process(Page page) {
             Selectable pageUrl = page.getUrl();
 
-            if (INDEX_URL.equals(pageUrl.toString())) {
+            if (pageUrl.toString().contains("index.html")) {
                 processProvince(page);
 
             } else if (pageUrl.regex(CITY_REGEX_URL).match()) {
@@ -65,10 +66,12 @@ public class CityCrawler {
             for (Selectable provinceTd : provinceTds) {
                 String url = provinceTd.xpath("//a/@href").toString();
                 String name = provinceTd.xpath("//a").regex(">(.+)<br>", 1).toString();
-                Area province = new Area(idGen.incrementAndGet(), name, ExecutiveLevel.PROVINCE);
-                page.addTargetRequest(new Request().setUrl(PREFIX_URL + url).putExtra("province", province));
-                // 将省份添加到返回值中
-                page.getResultItems().put(province.getName(), province);
+                if (name != null) {
+                    Area province = new Area(idGen.incrementAndGet(), name, ExecutiveLevel.PROVINCE);
+                    page.addTargetRequest(createRequest(PREFIX_URL + url).putExtra("province", province));
+                    // 将省份添加到返回值中
+                    page.getResultItems().put(province.getName(), province);
+                }
             }
         }
 
@@ -82,7 +85,7 @@ public class CityCrawler {
                 String name = node.xpath("//td[2]/a/text()").toString();
                 Area city = new Area(idGen.incrementAndGet(), name, code, ExecutiveLevel.CITY);
                 city.setParentArea(Area.class.cast(page.getRequest().getExtra("province")));
-                page.addTargetRequest(new Request().setUrl(PREFIX_URL + url).putExtra("city", city));
+                page.addTargetRequest(createRequest(PREFIX_URL + url).putExtra("city", city));
                 // 将城市添加到返回值中
                 page.getResultItems().put(city.getName(), city);
             }
@@ -108,5 +111,11 @@ public class CityCrawler {
             return site;
         }
 
+    }
+
+    private static Request createRequest(String url) {
+        Request request = new Request(url);
+        request.addHeader(HttpHeaders.USER_AGENT, "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36");
+        return request;
     }
 }
